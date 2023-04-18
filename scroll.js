0d612e17d4af4837c091b59511ebfdb7d97bb6a3
Rohan De graaf <52485505+ProfRedRooster@users.noreply.github.com>
2023-04-18 20:35:16 +0200

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -6,7 +6,7 @@ const copy = document.getElementById("copyExample");
 
 const frameCount = 148;
 const currentFrame = index => (
-  `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
+  `https://github.com/ProfRedRooster/Grieks-PO/raw/main/images/%20${index.toString().padStart(4, '0')}.jpg`
 )
 
 
