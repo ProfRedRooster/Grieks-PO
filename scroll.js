5414ff083aa2148a1c3e9ff51184f460e2357846
Rohan De graaf <52485505+ProfRedRooster@users.noreply.github.com>
2023-04-18 20:25:17 +0200

    Update and rename sticky.js to scroll.js

--- scroll.js
+++ scroll.js
@@ -8,14 +8,7 @@ const frameCount = 148;
 const currentFrame = index => (
   `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
 )
-const preloadImages = () => {
-  for (let i = 1; i < frameCount; i++) {
-    const img = new Image();
-    img.src = currentFrame(i);
-  }
-};
 
-preloadImages()
 
 const img = new Image()
 img.src = currentFrame(1);
